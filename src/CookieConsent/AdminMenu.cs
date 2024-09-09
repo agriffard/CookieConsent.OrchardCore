a8ad16d1c143f00fcf414ffe0895404558df47a6
Antoine Griffard <agriffard@hotmail.com>
2024-09-09 11:47:27 +0200

    AdminNavigationProvider

--- src/CookieConsent/AdminMenu.cs
+++ src/CookieConsent/AdminMenu.cs
@@ -1,34 +1,30 @@
-using System;
-using System.Threading.Tasks;
 using Microsoft.Extensions.Localization;
-using OrchardCore.Environment.Shell.Descriptor.Models;
 using OrchardCore.Navigation;
+using System.Threading.Tasks;
 
 namespace CookieConsent.OrchardCore
 {
-    public class AdminMenu : INavigationProvider
+    public class AdminMenu : AdminNavigationProvider
     {
         private readonly IStringLocalizer S;
 
-        public AdminMenu(
-            IStringLocalizer<AdminMenu> localizer)
+        public AdminMenu(IStringLocalizer<AdminMenu> localizer)
         {
             S = localizer;
         }
 
-        public ValueTask BuildNavigationAsync(string name, NavigationBuilder builder)
+        protected override ValueTask BuildAsync(NavigationBuilder builder)
         {
-            if (String.Equals(name, "admin", StringComparison.OrdinalIgnoreCase))
-            {
-                builder.Add(S["Configuration"], configuration => configuration
-                        .Add(S["Settings"], settings => settings
+            builder
+                .Add(S["Configuration"], configuration => configuration
+                    .Add(S["Settings"], settings => settings
                         .Add(S["Consent"], S["Consent"].PrefixPosition(), consent => consent
                             .AddClass("consent").Id("consent")
                             .Action("Index", "Admin", new { area = "OrchardCore.Settings", groupId = CookieConsentConstants.Features.CookieConsent })
                             .Permission(Permissions.ManageConsent)
                             .LocalNav())
-                    ));
-            }
+                ));
+
             return ValueTask.CompletedTask;
         }
     }
